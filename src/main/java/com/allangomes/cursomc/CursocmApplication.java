7b95d8e90916f1f9c9fcf119643c6bfd7b394a96
Allan Gomes Santos <allan11991@hotmail.com>
2018-09-09 20:30:24 -0300

    Projeto criado

---
+++ src/main/java/com/allangomes/cursomc/CursocmApplication.java
@@ -0,0 +1,12 @@
+package com.allangomes.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursocmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursocmApplication.class, args);
+	}
+}
